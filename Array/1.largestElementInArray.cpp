aaab8ad9eb6342c17f052486a4d8b0e670b5c35a
waghrahul60 <waghrahul60@gmail.com>
2020-06-28 12:27:35 +0530

    largest element

---
+++ Array/1.largestElementInArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[6]={5,8,3,4,1,9};
+    int Largest = arr[0];       //5
+
+    for(int i=0; i<=5; i++)
+    {
+        if(arr[i] > Largest)
+        {
+            Largest = arr[i];
+        }
+    }
+    cout<<"larhest Element Is :"<<Largest;
+    return 0;
+}
